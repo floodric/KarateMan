b44745f3e24577eeef932da34b76035dfaab5652
Ryan Flood <rflood@cmu.edu>
2013-05-03 10:51:07 -0400

    login functionality added, although no pages hidden yet

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Karate67272::Application.routes.draw do
   resources :tournaments
   resources :users
   
+
+  resources :sessions
   # Semi-static page routes
   match 'home' => 'home#index', :as => :home
   match 'about' => 'home#about', :as => :about
