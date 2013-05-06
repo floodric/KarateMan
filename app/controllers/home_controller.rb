9c8381e316b7179c2d5b88da915ac1659ead6f9e
Ryan Flood <rflood@cmu.edu>
2013-05-06 18:01:00 -0400

    fixing cucumber tests, removing redundant partials

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,11 +17,15 @@ class HomeController < ApplicationController
         # finalists partial
         require 'will_paginate/array'
         @done_tournament = Tournament.past.select{|t| t.registrations.select{|r| !r.final_standing.nil?}.size > 0}.first
-        sections = @done_tournament.sections.select{|s| s.registrations.select{|r| !r.final_standing.nil?}.size > 0}
-          # only get sections which have registrations with final standing 
-        finalists = sections.map{|s| s.registrations.by_final_standing.select{|r| !r.final_standing.nil?} }
-          # get only those who have a final standing
-        @result_sections = sections.zip(finalists).paginate(:page => params[:res_page], :per_page => 10)
+        if @done_tournament.nil? # no past tournaments with final_standings
+          @result_sections=[]
+        else # get array of [section,registration list of winners]
+          sections = @done_tournament.sections.select{|s| s.registrations.select{|r| !r.final_standing.nil?}.size > 0}
+            # only get sections which have registrations with final standing 
+          finalists = sections.map{|s| s.registrations.by_final_standing.select{|r| !r.final_standing.nil?} }
+            # get only those who have a final standing
+          @result_sections = sections.zip(finalists).paginate(:page => params[:res_page], :per_page => 10)
+        end
 
 
       else
